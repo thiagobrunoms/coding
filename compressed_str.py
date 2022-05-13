9a80d8df188f6c92b959de36551501f5b600d798
Thiago de Sales <thiagobrunoms@gmail.com>
2022-05-13 19:01:52 -0300

    Improves some checks

--- compressed_str.py
+++ compressed_str.py
@@ -1,28 +1,26 @@
+# String Compression: Implement a method to perform basic string compression using the counts
+# of repeated characters. For example, the string aabcccccaaa would become a2b1c5a3. If the
+# "compressed" string would not become smaller than the original string, your method should return
+# the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+# Hints: #92, # 11 0
+
 class Solution:
     def compress_string(self, content: str) -> str:
         compressed = ""
-
-        previous_letter = content[0]
         count_letter = 0
-        for i in range(1, len(content)):
-            current_letter = content[i]
-            
-            if current_letter == previous_letter:
-                count_letter += 1
-            else:
-                count_letter += 1
-                compressed = compressed + previous_letter + str(count_letter)
-                count_letter = 0
-                    
-            previous_letter = current_letter
 
-
-        if count_letter > 0 or previous_letter == current_letter:
+        for i in range(len(content)):
             count_letter += 1
-            compressed = compressed + current_letter + str(count_letter)
 
-        return compressed
+            if i + 1 >= len(content) or content[i] != content[i + 1]:
+                compressed = compressed + content[i] + str(count_letter)
+                count_letter = 0
+
+        return compressed if len(compressed) < len(content) else content
 
 s = Solution()
 result = s.compress_string("abcccrcccaarraabbddddce")
+print(result)
+
+result = s.compress_string("abcccrcccaarraabbdddd")
 print(result)
