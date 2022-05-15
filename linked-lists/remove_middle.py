589b3a486cc500a7ee1860e957bd5f385e903de7
Thiago de Sales <thiagobrunoms@gmail.com>
2022-05-14 21:42:43 -0300

    Removes middle node from a linked link

--- linked-lists/remove_middle.py
+++ linked-lists/remove_middle.py
@@ -1,7 +1,10 @@
-# Remove Dups: Write code to remove duplicates from an unsorted linked list.
-# FOLLOW UP
-# How would you solve this problem if a temporary buffer is not allowed?
-# Hints: #9, #40
+# Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
+# the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+# that node.
+# EXAMPLE
+# Input: the node c from the linked list a - >b- >c - >d - >e- >f
+# Result: nothing is returned, but the new linked list looks like a - >b- >d - >e- >f
+# Hints: #72
 
 class Node:
     def __init__(self, node_id: int):
@@ -18,14 +21,26 @@ class LinkedList:
         else:
             self.head = head
 
+        self.middle_node = self.head
+        self.current_index = 0
+
     def add(self, n: Node):
+        self.current_index += 1
+
         if self.head.next == None:
             self.head.next = n
             return
-        
+
+        #1 -> 2 -> 3 ->
         currentNode = self.head
+        count = 0
         while (currentNode.next != None):
             currentNode = currentNode.next
+            count += 1
+
+            if count == self.current_index // 2:
+                self.middle_node = currentNode
+                print("middle", self.middle_node)
 
         currentNode.next = n
 
@@ -39,21 +54,20 @@ class LinkedList:
 
         return current_node
 
-    def remove_duplicates_with_buffer(self):
-        structure = {}
-
+    def delete_middle(self):
         current_node = self.head
         previous_node = self.head
-        while (current_node != None):
-            if current_node.node_id in structure:
-                previous_node.next = current_node.next
-            else:
-                structure[current_node.node_id] = True
-                previous_node = current_node
-            
-            current_node = current_node.next
 
-        
+        while current_node != None:
+            if current_node.node_id == self.middle_node.node_id:
+                previous_node.next = current_node.next
+                break
+            else:
+                previous_node = current_node
+                current_node = current_node.next
+
+        self.current_index -= 1
+
     def __str__(self):
         content = self.head.__str__()
 
@@ -64,22 +78,18 @@ class LinkedList:
 
         return content
 
+#1->2->3->4->5->6
 linkedList = LinkedList(Node(1))
 # print(linkedList)
 linkedList.add(Node(2))
 linkedList.add(Node(3))
-linkedList.add(Node(3))
-linkedList.add(Node(4))
-linkedList.add(Node(3))
 linkedList.add(Node(4))
 linkedList.add(Node(5))
+linkedList.add(Node(6))
+linkedList.add(Node(7))
 
 print(linkedList)
 
-linkedList.remove_duplicates_with_buffer()
+linkedList.delete_middle()
 
 print(linkedList)
-
-
-# class Solution:
-#     pass
