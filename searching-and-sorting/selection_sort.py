f90dcfe4160768a9c7c895f8fcaf5c7004a1cc34
Thiago de Sales <thiagobrunoms@gmail.com>
2022-06-16 09:39:17 -0300

    Selection sort algorithm

---
+++ searching-and-sorting/selection_sort.py
@@ -0,0 +1,16 @@
+
+def selection_sort_it(elements: list) -> list:
+    for i in range(len(elements)):
+        min_index = i
+        for j in range(i+1, len(elements)):
+            if elements[min_index] > elements[j]:
+                min_index = j
+
+        temp = elements[i]
+        elements[i] = elements[min_index]
+        elements[min_index] = temp
+
+    return elements
+
+unorded_list = selection_sort_it([73, 62, 61, 69, 10, 25, 100, 50])
+print(unorded_list)
