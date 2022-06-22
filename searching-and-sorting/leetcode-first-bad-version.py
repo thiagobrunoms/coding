fc0cfdfdb6a5e624da15844fcc3dbd7823d9568e
Thiago de Sales <thiagobrunoms@gmail.com>
2022-06-22 15:29:01 -0300

    Finds the first bad version of a series of bad versions. This solution uses binary search

---
+++ searching-and-sorting/leetcode-first-bad-version.py
@@ -0,0 +1,64 @@
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+
+# Example 1:
+
+# Input: n = 5, bad = 4
+# Output: 4
+# Explanation:
+# call isBadVersion(3) -> false
+# call isBadVersion(5) -> true
+# call isBadVersion(4) -> true
+# Then 4 is the first bad version.
+# Example 2:
+
+# Input: n = 1, bad = 1
+# Output: 1
+
+# Constraints:
+
+#1 <= bad <= n <= 231 - 1
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+from operator import is_
+import random
+
+
+def isBadVersion(version: int) -> bool:
+    if version >= 4:
+        return True
+
+    return False
+
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        if n == 1:
+            return 1
+
+        left = 0
+        right = n
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            is_bad = isBadVersion(mid)
+
+            if is_bad:
+                right = mid
+            else:
+                if mid + 1 == right:
+                    return right
+
+                left = mid
+
+
+s = Solution()
+result = s.firstBadVersion(5)
+print(result)
