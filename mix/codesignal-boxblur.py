8487968e0ff564b63be4f8cd674860bb4b01d55e
Thiago de Sales <thiagobrunoms@gmail.com>
2022-07-21 12:28:22 -0300

    Box Blur - fix row counting and removes uneeded max_row updates

--- mix/codesignal-boxblur.py
+++ mix/codesignal-boxblur.py
@@ -3,37 +3,34 @@ from email.mime import image
 
 def solution(image):
     result = []
+
     row = 0
     max_row = 3
     column = 0
     max_column = 3
 
-    controller = 0
     shift_row = 0
     shift_column = 0
 
     new_point = []
     while max_row <= len(image):
-        # print('starting')
+        print('starting', 'maxrow', max_row, 'max_column', max_column)
         sum = 0
 
         for i in range(row, max_row):
             shift_row = i
             for j in range(column, max_column):
-                # print(i, j)
+                print(i, j)
                 shift_column = j
                 sum += image[i][j]
 
         new_average = sum // 9
-        print('new_average', new_average)
+        # print('new_average', new_average)
 
         new_point.append(new_average)
         if shift_column < len(image[0]) - 1:
             column += 1
-            row = shift_row - shift_column
-
             max_column += 1
-            max_row = row + 3
         else:
             result.append(new_point)
             new_point = []
@@ -51,7 +48,15 @@ def solution(image):
 # image = [[1, 1, 1], [1, 7, 1], [1, 1, 1]]
 # image = [[0, 18, 9], [27, 9, 0],  [81, 63, 45]]
 # image = [[36, 0, 18, 9], [27, 54, 9, 0], [81, 63, 72, 45]]
-image = [[36,0,18,9,9,45,27], [27,0,54,9,0,63,90], [81,63,72,45,18,27,0], [0,0,9,81,27,18,45], [45,45,27,27,90,81,72], [45,18,9,0,9,18,45], [27,81,36,63,63,72,81]]
+# image = [[36, 0, 18, 9, 9, 45, 27], [27, 0, 54, 9, 0, 63, 90], [81, 63, 72, 45, 18, 27, 0], [
+#     0, 0, 9, 81, 27, 18, 45], [45, 45, 27, 27, 90, 81, 72], [45, 18, 9, 0, 9, 18, 45], [27, 81, 36, 63, 63, 72, 81]]
+image = [[36, 0, 18, 9, 9, 45, 27],
+         [27, 0, 254, 9, 0, 63, 90],
+         [81, 255, 72, 45, 18, 27, 0],
+         [0, 0, 9, 81, 27, 18, 45],
+         [45, 45, 227, 227, 90, 81, 72],
+         [45, 18, 9, 255, 9, 18, 45],
+         [27, 81, 36, 127, 255, 72, 81]]
 s = solution(image)
 
 print(s)
