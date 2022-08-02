dba0e968840a603a2e838fae2f04f18243e5d1fb
Thiago de Sales <thiagobrunoms@gmail.com>
2022-08-02 08:53:16 -0300

    Delete one digit to find the highest number

---
+++ mix/codesignal-delete-digit.py
@@ -0,0 +1,24 @@
+def solution(n):
+    digits = []
+    while n > 0:
+        digit = n % 10
+        n = n // 10
+        digits.insert(0, digit)
+
+    power = len(digits) - 2
+    current_removed = 0
+    result = 0
+    number = 0
+    for remove_index_at in range(len(digits)):
+        for each_digit_index in range(len(digits)):
+            if remove_index_at != each_digit_index:
+                number += digits[each_digit_index] * (10 ** power)
+                power -= 1
+
+        if number > result:
+            result = number
+
+        power = len(digits) - 2
+        number = 0
+
+    return result
