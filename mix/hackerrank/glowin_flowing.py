52e3003974378ffd2d8771565baab161b1aca04e
Thiago de Sales <thiagobrunoms@gmail.com>
2022-08-16 12:09:28 -0300

    Consider cases where tanks get filled in half of a time, since a tank ith gets water from the system and from the ith-1 tank

--- mix/hackerrank/glowin_flowing.py
+++ mix/hackerrank/glowin_flowing.py
@@ -1,3 +1,6 @@
+import math
+
+
 def fill_and_get(amount, content_in_tanks, pos, capacity):
     content_in_tanks[pos] += amount
     if content_in_tanks[pos] > capacity:
@@ -13,7 +16,8 @@ def solution(k, n_tanks, capacities):
     content_in_tanks = [0] * n_tanks
     last_overflows_at = 0
     all_overflows_at = 0
-    time = 1
+    time = 1/10
+    k = k / 10
     remainder = 0
     while not is_full:
         for i in range(len(content_in_tanks)):
@@ -29,13 +33,26 @@ def solution(k, n_tanks, capacities):
                 is_full = False
                 break
 
-        time += 1
+        if not is_full:
+            time += 0.1
+
         all_overflows_at = time
         remainder = 0
 
-    return (last_overflows_at, all_overflows_at - 1)
+    if last_overflows_at - int(last_overflows_at) <= 0.6:
+        last_overflows_at = math.floor(last_overflows_at)
+    else:
+        last_overflows_at = math.ceil(last_overflows_at)
+
+    if all_overflows_at - int(all_overflows_at) <= 0.6:
+        all_overflows_at = math.floor(all_overflows_at)
+    else:
+        all_overflows_at = math.ceil(all_overflows_at)
+
+    return (last_overflows_at, all_overflows_at)
 
 
+# s = solution(2, 2, [4, 6])
 s = solution(1, 4, [30, 3, 7, 20])
 print('last overflows at', s[0])
 print('all overflows at', s[1])
