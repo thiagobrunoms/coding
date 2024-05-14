63628db66ad76ddceb82fc558ca85e2d98d90060
Thiago Sales <thiagobrunoms@gmail.com>
2024-05-14 11:17:24 -0300

    Add post and pre-order tree traversal

--- trees-graphs/binary-tree.py
+++ trees-graphs/binary-tree.py
@@ -6,15 +6,21 @@ class BinaryTreeNode:
         self.right: BinaryTreeNode = None
 
     def insert(self, value):
+        print('adding', value, 'where self.left', self.left.value if self.left and self.left.value is not None else 'None', 'and self.right', self.right.value if self.right and self.right.value is not None else 'None')
+
         if value < self.value:
             if self.left is None:
                 self.left = BinaryTreeNode(value)
+                print('added ', value, 'as left from')
             else:
+                print('recursing left', self.left.value)
                 self.left.insert(value)
         else:
             if self.right is None:
+                print('added ', value, 'as right ')
                 self.right = BinaryTreeNode(value)
             else:
+                print('recursing right', self.right.value)
                 self.right.insert(value)
 
     def in_order_traversal(self, node):
@@ -23,12 +29,31 @@ class BinaryTreeNode:
             print(node.value)
             self.in_order_traversal(node.right)
 
-binaryTree = BinaryTreeNode(10)
-binaryTree.insert(7)
-binaryTree.insert(12)
-binaryTree.insert(5)
-binaryTree.insert(6)
-binaryTree.insert(11)
-binaryTree.insert(13) 
+    def post_order_traversal(self, node):
+        if (node):
+            self.post_order_traversal(node.left)
+            self.post_order_traversal(node.right)
+            print(node.value)
 
+    def pre_order_traversal(self, node):
+        if (node):
+            print(node.value)
+            self.pre_order_traversal(node.left)
+            self.pre_order_traversal(node.right)
+
+binaryTree = BinaryTreeNode(10)
+binaryTree.insert(5)
+binaryTree.insert(13)
+binaryTree.insert(6)
+binaryTree.insert(7)
+binaryTree.insert(11)
+binaryTree.insert(12) 
+
+print('in order traversal - as left as possible - left -> root -> right')
 binaryTree.in_order_traversal(binaryTree)
+
+print('post order traversal -> left -> right -> root')
+binaryTree.post_order_traversal(binaryTree)
+
+print('pre order traversal -> root -> left -> right')
+binaryTree.pre_order_traversal(binaryTree)
