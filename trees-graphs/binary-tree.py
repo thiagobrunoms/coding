8686ae7eceb736909b15a62e28c65c194af73b33
Thiago Sales <thiagobrunoms@gmail.com>
2024-05-14 10:06:51 -0300

    Fix binary tree representation

--- trees-graphs/binary-tree.py
+++ trees-graphs/binary-tree.py
@@ -1,44 +1,34 @@
 
-class BinaryTree:
+class BinaryTreeNode:
     def __init__(self, value):
         self.value = value
-        self.left: BinaryTree = None
-        self.right: BinaryTree = None
+        self.left: BinaryTreeNode = None
+        self.right: BinaryTreeNode = None
 
     def insert(self, value):
-        node = BinaryTree(value)
-
-        if node.value < value:
+        if value < self.value:
             if self.left is None:
-                self.left = node
+                self.left = BinaryTreeNode(value)
             else:
-                self.insert_left(node)
+                self.left.insert(value)
         else:
             if self.right is None:
-                self.right = node
+                self.right = BinaryTreeNode(value)
             else:
-                self.insert_right(node)
+                self.right.insert(value)
 
-    def insert_left(self, node):
-        node.left = self.left
-        self.left = node
+    def in_order_traversal(self, node):
+        if (node):
+            self.in_order_traversal(node.left)
+            print(node.value)
+            self.in_order_traversal(node.right)
 
-    def insert_right(self, node):
-        node.right = self.right
-        self.right = node
-
-    def in_order_traversal(self):
-        if self.left:
-            self.left.in_order_traversal()
-
-        print(self.value)
-
-        if self.right:
-            self.right.in_order_traversal()
-
-binaryTree = BinaryTree(10)
+binaryTree = BinaryTreeNode(10)
 binaryTree.insert(7)
 binaryTree.insert(12)
-binaryTree.insert(5)            
+binaryTree.insert(5)
+binaryTree.insert(6)
+binaryTree.insert(11)
+binaryTree.insert(13) 
 
-binaryTree.in_order_traversal()
+binaryTree.in_order_traversal(binaryTree)
